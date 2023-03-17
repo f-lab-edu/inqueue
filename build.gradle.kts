ca0472d33dbb980ca75a3fef9ac7bd270770aee6
Jiwon Kim <jwkim.oa@gmail.com>
2023-03-17 11:15:57 +0900

    프로젝트 설정 수정(mysql, jpa 설정 추가) - Issue #2

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,11 @@ repositories {
 }
 
 dependencies {
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-//    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
