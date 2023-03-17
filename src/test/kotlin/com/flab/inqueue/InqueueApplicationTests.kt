305ae1013ea77eb3abd2e27af64bde0644e72582
Jiwon Kim <jwkim.oa@gmail.com>
2023-03-17 11:00:08 +0900

    프로젝트 셋팅 - Issue #2

---
+++ src/test/kotlin/com/flab/inqueue/InqueueApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.flab.inqueue
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InqueueApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
