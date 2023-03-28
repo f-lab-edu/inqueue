eaf4698fc081c4e90f703420c35e346b572ec669
elyo9381 <won4413@daum.net>
2023-03-28 17:00:59 +0900

    build : rest-assured 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
+
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
