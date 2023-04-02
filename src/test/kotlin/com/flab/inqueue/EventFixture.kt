6438e8ef909411f1216c8b311c476633681a81d9
elyo9381 <won4413@daum.net>
2023-04-02 22:28:25 +0900

    test fix : eventFixture 추가

---
+++ src/test/kotlin/com/flab/inqueue/EventFixture.kt
@@ -0,0 +1,42 @@
+package com.flab.inqueue
+
+import com.flab.inqueue.domain.event.dto.EventRequest
+import com.flab.inqueue.domain.event.entity.Event
+import com.flab.inqueue.domain.event.entity.EventInformation
+import java.time.LocalDateTime
+
+
+fun createEvent(
+    eventId: String? = null,
+    waitQueueStartDateTime: LocalDateTime = LocalDateTime.now(),
+    waitQueueEndDateTime: LocalDateTime = LocalDateTime.now().plusDays(2),
+    jobQueueSize: Long = 10,
+    jobQueueLimitTime: Long = 10L,
+    eventinfo : EventInformation = EventInformation(),
+    redirectUrl: String? =  "http://inqueue.test.com"
+): Event {
+    return createEventRequest(
+        eventId,
+        waitQueueStartDateTime,
+        waitQueueEndDateTime,
+        jobQueueSize,
+        jobQueueLimitTime,
+        eventinfo,
+        redirectUrl
+    ).toEntity()
+}
+
+
+
+
+fun createEventRequest(
+    eventId : String?= null,
+    waitQueueStartDateTime: LocalDateTime = LocalDateTime.now(),
+    waitQueueEndDateTime: LocalDateTime = LocalDateTime.now().plusDays(2),
+    jobQueueSize: Long = 10,
+    jobQueueLimitTime: Long = 10L,
+    eventinfo : EventInformation = EventInformation(),
+    redirectUrl: String? = "http://inqueue.test.com"
+): EventRequest {
+    return EventRequest(eventId,waitQueueStartDateTime,waitQueueEndDateTime,jobQueueSize,jobQueueLimitTime,eventinfo,redirectUrl)
+}
