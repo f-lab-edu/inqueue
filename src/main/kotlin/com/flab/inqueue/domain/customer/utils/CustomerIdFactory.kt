c32962b188c44e1bd657ca5491a2cd02b6602d30
JKjiwon <jwkim.oa@gmail.com>
2023-04-03 14:49:31 +0900

    feat : customer의 clientId, clientSecret 발급 유틸 추가

---
+++ src/main/kotlin/com/flab/inqueue/domain/customer/utils/CustomerIdFactory.kt
@@ -0,0 +1,23 @@
+package com.flab.inqueue.domain.customer.utils
+
+import java.security.SecureRandom
+import java.util.*
+
+object CustomerIdFactory {
+
+    private const val DEFAULT_CLIENT_ID_LENGTH = 32
+
+    private const val DEFAULT_CLIENT_SECRET_LENGTH = 64
+
+    fun generateClientId(): String {
+        val bytes = ByteArray(DEFAULT_CLIENT_ID_LENGTH)
+        SecureRandom().nextBytes(bytes)
+        return Base64.getEncoder().encodeToString(bytes)
+    }
+
+    fun generateClientSecret(): String? {
+        val bytes = ByteArray(DEFAULT_CLIENT_SECRET_LENGTH)
+        SecureRandom().nextBytes(bytes)
+        return Base64.getEncoder().encodeToString(bytes)
+    }
+}
