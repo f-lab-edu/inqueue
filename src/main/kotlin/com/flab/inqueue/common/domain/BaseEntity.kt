f6aeae7cf1a749a0903f4c06e323cf9a69d77748
elyo9381 <won4413@daum.net>
2023-04-04 09:53:12 +0900

    fix BaseEntity public 명시

--- src/main/kotlin/com/flab/inqueue/common/domain/BaseEntity.kt
+++ src/main/kotlin/com/flab/inqueue/common/domain/BaseEntity.kt
@@ -6,7 +6,7 @@ import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
 
 @MappedSuperclass
-abstract class BaseEntity(
+public abstract class BaseEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L
