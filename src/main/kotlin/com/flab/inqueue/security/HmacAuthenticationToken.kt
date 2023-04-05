540be98764b16ac4c854c1046270a15a16775c99
JKjiwon <jwkim.oa@gmail.com>
2023-04-06 02:30:04 +0900

    feat : HmacSignature 인증을 HmacAuthenticationProvider 통하여 하도록 수정

---
+++ src/main/kotlin/com/flab/inqueue/security/HmacAuthenticationToken.kt
@@ -0,0 +1,56 @@
+package com.flab.inqueue.security
+
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+
+class HmacAuthenticationToken(
+    val clientId: String? = null,
+    val signature: String? = null,
+    val payload: String? = null,
+    private val authorities: MutableCollection<out GrantedAuthority> = mutableListOf()
+) : Authentication {
+
+    private var principal = clientId
+    private var isAuthenticated = false
+
+    companion object {
+        @JvmStatic
+        fun authenticatedToken(
+            clientId: String,
+            authorities: MutableCollection<out GrantedAuthority>
+        ): HmacAuthenticationToken {
+            return HmacAuthenticationToken(
+                clientId = clientId,
+                authorities = authorities
+            ).apply { isAuthenticated = true }
+        }
+    }
+
+    override fun getName(): String {
+        return this.clientId!!
+    }
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return this.authorities
+    }
+
+    override fun getCredentials(): Any? {
+        return null
+    }
+
+    override fun getDetails(): Any? {
+        return null
+    }
+
+    override fun getPrincipal(): Any? {
+        return principal
+    }
+
+    override fun isAuthenticated(): Boolean {
+        return this.isAuthenticated
+    }
+
+    override fun setAuthenticated(isAuthenticated: Boolean) {
+        this.isAuthenticated = isAuthenticated
+    }
+}
