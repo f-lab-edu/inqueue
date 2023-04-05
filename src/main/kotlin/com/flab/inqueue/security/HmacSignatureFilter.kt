9903e4ee957a0ac25e67b4ca524040efcd941552
JKjiwon <jwkim.oa@gmail.com>
2023-04-06 02:30:04 +0900

    feat : HmacSignature 인증을 HmacAuthenticationProvider 통하여 하도록 수정

--- src/main/kotlin/com/flab/inqueue/security/HmacSignatureFilter.kt
+++ src/main/kotlin/com/flab/inqueue/security/HmacSignatureFilter.kt
@@ -4,38 +4,36 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpHeaders
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.core.AuthenticationException
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 
-@Component
 class HmacSignatureFilter(
-    private val customerUserDetailsService: CustomerUserDetailsService,
-    private val hmacSignatureVerifier: HmacSignatureVerifier
-
+    private val authenticationManager: AuthenticationManager
 ) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
-        request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
     ) {
-        val authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION)
+        val authorization = request.getHeader(HttpHeaders.AUTHORIZATION)
 
-        if (authorizationHeader.isNullOrEmpty() || !authorizationHeader.contains(":")) {
+        if (authorization.isNullOrEmpty() && !authorization.contains(":")) {
             filterChain.doFilter(request, response)
             return
         }
 
-        val (clientId, signature) = authorizationHeader.split(":")
-        val userDetails: UserDetails = customerUserDetailsService.loadUserByUsername(clientId)
-
-        if (hmacSignatureVerifier.verify(signature, userDetails.password, request.requestURL.toString())) {
-            val authentication = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
-            SecurityContextHolder.getContext().authentication = authentication
+        try {
+            val (clientId, signature) = authorization.split(":")
+            val hmacAuthenticationToken = HmacAuthenticationToken(clientId, signature, request.requestURL.toString())
+            val result = authenticationManager.authenticate(hmacAuthenticationToken)
+            SecurityContextHolder.getContext().authentication = result
+        } catch (e: AuthenticationException) {
+            SecurityContextHolder.clearContext()
         }
-
         filterChain.doFilter(request, response)
     }
 }
--- src/main/kotlin/com/flab/inqueue/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/flab/inqueue/security/WebSecurityConfig.kt
@@ -2,32 +2,42 @@ package com.flab.inqueue.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
+
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig(
-    val hmacSignatureFilter: HmacSignatureFilter
+    private val hmacAuthenticationProvider : AuthenticationProvider
 ) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .csrf().disable()
+            .cors().disable()
             .authorizeHttpRequests()
-            .requestMatchers("/v1/auth/**")
-            .authenticated()
-            .anyRequest()
-            .permitAll()
+            .requestMatchers("/v1/auth/**").authenticated()
+            .anyRequest().permitAll()
         http
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         http
-            .addFilterBefore(hmacSignatureFilter, UsernamePasswordAuthenticationFilter::class.java)
-
+            .addFilterAfter(
+                hmacSignatureFilter(),
+                UsernamePasswordAuthenticationFilter::class.java
+            )
         return http.build()
+    }
+
+    @Bean
+    fun hmacSignatureFilter() : HmacSignatureFilter {
+        val authenticationManager = ProviderManager(hmacAuthenticationProvider)
+        return HmacSignatureFilter(authenticationManager)
     }
 }
