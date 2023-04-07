cb2e217e7e045941f55a864c9d6ea08aea6f65d4
JKjiwon <jwkim.oa@gmail.com>
2023-04-07 21:46:50 +0900

    fix : Hmac 인증 패키지 변경

--- src/main/kotlin/com/flab/inqueue/security/hmacsinature/HmacAuthenticationToken.kt
+++ src/main/kotlin/com/flab/inqueue/security/hmacsinature/HmacAuthenticationToken.kt
@@ -1,4 +1,4 @@
-package com.flab.inqueue.security
+package com.flab.inqueue.security.hmacsinature
 
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
