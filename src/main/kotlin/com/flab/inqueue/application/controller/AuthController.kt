f61585ed366cf7915ab25270b9517ede2ec4ac28
JKjiwon <jwkim.oa@gmail.com>
2023-04-07 21:50:20 +0900

    fix : 토큰 발급 API 헤더 제거

--- src/main/kotlin/com/flab/inqueue/application/controller/AuthController.kt
+++ src/main/kotlin/com/flab/inqueue/application/controller/AuthController.kt
@@ -4,7 +4,6 @@ import com.flab.inqueue.domain.dto.AuthRequest
 import com.flab.inqueue.domain.dto.AuthResponse
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,7 +13,6 @@ class AuthController {
 
     @PostMapping("/token")
     fun generateToken(
-        @RequestHeader("Authorization") accessKey: String,
         @RequestBody authRequest: AuthRequest
     ): AuthResponse {
         return AuthResponse("JWT Token")
