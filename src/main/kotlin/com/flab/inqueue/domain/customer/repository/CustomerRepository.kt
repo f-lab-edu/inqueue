fa82be37333c39198aa27f7ad0024dcbff421c9a
JKjiwon <jwkim.oa@gmail.com>
2023-04-08 19:19:55 +0900

    fix : customer role 을 테이블로 관리, @ElementCollection 을 통한 조회

--- src/main/kotlin/com/flab/inqueue/domain/customer/repository/CustomerRepository.kt
+++ src/main/kotlin/com/flab/inqueue/domain/customer/repository/CustomerRepository.kt
@@ -1,10 +1,12 @@
 package com.flab.inqueue.domain.customer.repository
 
 import com.flab.inqueue.domain.customer.entity.Customer
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CustomerRepository : JpaRepository<Customer, Long> {
 
+    @EntityGraph(attributePaths = ["roles"])
     fun findByClientId(clientId: String): Customer?
 
 }
