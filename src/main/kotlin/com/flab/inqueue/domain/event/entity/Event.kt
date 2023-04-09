33c4266e3f7c6acc844a926bd31d0ab80344b74d
elyo9381 <won4413@daum.net>
2023-04-09 16:41:40 +0900

    fix : 복사생성자 생성 , update시 entity 객체 받는것으로 변경

--- src/main/kotlin/com/flab/inqueue/domain/event/entity/Event.kt
+++ src/main/kotlin/com/flab/inqueue/domain/event/entity/Event.kt
@@ -16,34 +16,38 @@ class Event(
     @Column(nullable = false)
     val eventId: String, // 필수이며, 임뮤터블 이라고 생각
 
-    period : WaitQueuePeriod, // 필수이지만 , 뮤터블 하다고 생각해서 var를 선언, 대신 @Column으로 nullable 처리 토록 작성
+    period: WaitQueuePeriod, // 필수이지만 , 뮤터블 하다고 생각해서 var를 선언, 대신 @Column으로 nullable 처리 토록 작성
     jobQueueSize: Long,
     jobQueueLimitTime: Long,
 
-    eventInfo : EventInformation = EventInformation(), // 필수가 아니여서 초기화 진행
+    eventInfo: EventInformation = EventInformation(), // 필수가 아니여서 초기화 진행
     redirectUrl: String? = null,
 
     @Column(nullable = false, updatable = false)
     val createdDateTime: LocalDateTime = LocalDateTime.now(),
     modifiedDateTime: LocalDateTime = LocalDateTime.now(),
 
-    id:Long
-) : BaseEntity(id){
+    id: Long,
+) : BaseEntity(id) {
 
     @Embedded
-    var period : WaitQueuePeriod = period
+    var period: WaitQueuePeriod = period
         private set
+
     @Column(nullable = false)
     var jobQueueSize: Long = jobQueueSize
         private set
+
     @Column(nullable = false)
     var jobQueueLimitTime: Long = jobQueueLimitTime
         private set
+
     @Embedded
-    var eventInfo : EventInformation = eventInfo
+    var eventInfo: EventInformation = eventInfo
         private set
     var redirectUrl: String? = redirectUrl
         private set
+
     @Column(nullable = false)
     var modifiedDateTime: LocalDateTime = modifiedDateTime
         private set
@@ -54,26 +58,37 @@ class Event(
         waitQueueEndDateTime: LocalDateTime,
         jobQueueSize: Long,
         jobQueueLimitTime: Long,
-        eventinfo : EventInformation,
-        redirectUrl: String?
-    ) : this(eventId,WaitQueuePeriod(waitQueueStartDateTime, waitQueueEndDateTime),jobQueueSize,jobQueueLimitTime, id = 0L) {
+        eventinfo: EventInformation,
+        redirectUrl: String?,
+    ) : this(
+        eventId,
+        WaitQueuePeriod(waitQueueStartDateTime, waitQueueEndDateTime),
+        jobQueueSize,
+        jobQueueLimitTime,
+        id = 0L
+    ) {
         this.eventInfo = eventinfo
         this.redirectUrl = redirectUrl
     }
 
-    fun update(
-        waitQueueStartDateTime: LocalDateTime,
-        waitQueueEndDateTime: LocalDateTime,
-        jobQueueSize: Long,
-        jobQueueLimitTime: Long,
-        eventInfo : EventInformation,
-        redirectUrl: String?
-    ) {
-        this.period = WaitQueuePeriod(waitQueueStartDateTime,waitQueueEndDateTime)
-        this.jobQueueSize = jobQueueSize
-        this.jobQueueLimitTime = jobQueueLimitTime
-        this.eventInfo = eventInfo
-        this.redirectUrl = redirectUrl
+    fun update( event: Event ) {
+        this.period = event.period
+        this.jobQueueSize = event.jobQueueSize
+        this.jobQueueLimitTime = event.jobQueueLimitTime
+        this.eventInfo = event.eventInfo
+        this.redirectUrl = event.redirectUrl
         this.modifiedDateTime = LocalDateTime.now()
     }
+    companion object {
+        fun from(event: Event) = Event(
+            event.eventId,
+            event.period.startDateTime,
+            event.period.endDateTime,
+            event.jobQueueSize,
+            event.jobQueueLimitTime,
+            event.eventInfo,
+            event.redirectUrl,
+        )
+    }
+
 }
