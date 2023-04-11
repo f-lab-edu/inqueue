b5bd6605acf4908ee9921f2fdaae39e4c352e822
JKjiwon <jwkim.oa@gmail.com>
2023-04-11 21:14:51 +0900

    feat : 정적 자원은 스프링 시큐리티 필터 제외

--- src/main/kotlin/com/flab/inqueue/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/flab/inqueue/security/WebSecurityConfig.kt
@@ -1,12 +1,15 @@
 package com.flab.inqueue.security
 
 import com.flab.inqueue.security.hmacsinature.HmacSignatureFilter
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
@@ -45,5 +48,22 @@ class WebSecurityConfig(
     fun hmacSignatureFilter(): HmacSignatureFilter {
         val authenticationManager = ProviderManager(hmacAuthenticationProvider)
         return HmacSignatureFilter(authenticationManager)
+    }
+
+    @Profile("dev")
+    @Bean
+    fun devWebSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web ->
+            web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .requestMatchers(PathRequest.toH2Console())
+        }
+    }
+
+    @Profile("prod")
+    @Bean
+    fun prodWebSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web ->
+            web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+        }
     }
 }
