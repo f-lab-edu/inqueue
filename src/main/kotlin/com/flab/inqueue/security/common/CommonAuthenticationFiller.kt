d730da49204ef287b7eed613d18d0131b4e27cb2
JKjiwon <jwkim.oa@gmail.com>
2023-04-12 22:00:24 +0900

    feat : requestMatcher 를 포함한 공통 필터 추가

---
+++ src/main/kotlin/com/flab/inqueue/security/common/CommonAuthenticationFiller.kt
@@ -0,0 +1,41 @@
+package com.flab.inqueue.security.common
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.util.matcher.RequestMatcher
+import org.springframework.web.filter.OncePerRequestFilter
+
+abstract class CommonAuthenticationFiller(
+    protected val authenticationManager: AuthenticationManager,
+    private vararg val requestMatchers: RequestMatcher
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        try {
+            if (!requiresAuthentication(request, response)) {
+                filterChain.doFilter(request, response)
+                return
+            }
+
+            val result = attemptAuthentication(request, response)
+            SecurityContextHolder.getContext().authentication = result
+        } catch (e: AuthenticationException) {
+            SecurityContextHolder.clearContext()
+        }
+        filterChain.doFilter(request, response)
+    }
+
+    protected open fun requiresAuthentication(request: HttpServletRequest, response: HttpServletResponse): Boolean {
+        return requestMatchers.any{ requestMatcher -> requestMatcher.matches(request) }
+    }
+    abstract fun attemptAuthentication(request: HttpServletRequest, response: HttpServletResponse): Authentication
+
+}
