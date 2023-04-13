10189c40678b7dc01cdecaf35992a947e273d7b3
elyo9381 <won4413@daum.net>
2023-04-13 18:42:24 +0900

    fix : configurationProperties

--- src/main/kotlin/com/flab/inqueue/infra/property/RedisProperty.kt
+++ src/main/kotlin/com/flab/inqueue/infra/property/RedisProperty.kt
@@ -1,12 +1,9 @@
 package com.flab.inqueue.infra.property
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
-@Configuration
-@ConfigurationProperties("spring.data.redis")
+@ConfigurationProperties(prefix = "spring.data.redis")
 data class RedisProperty(
-    val host: String = "",
-    val port: Int = 0,
-    val password: String = "",
+    val host: String,
+    val port: Int,
 )
