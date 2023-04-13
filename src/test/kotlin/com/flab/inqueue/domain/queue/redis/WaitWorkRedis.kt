337d8e169b65ae4b95fb32d10466dc845e8375d7
elyo9381 <won4413@daum.net>
2023-04-13 18:44:08 +0900

    test fix : testcontainer 분리 및 런타임시 동적으로 redis port 및host 설정 ,테스트를 위한 property 설정

--- src/test/kotlin/com/flab/inqueue/domain/queue/redis/WaitWorkRedis.kt
+++ src/test/kotlin/com/flab/inqueue/domain/queue/redis/WaitWorkRedis.kt
@@ -1,10 +1,11 @@
 package com.flab.inqueue.domain.queue.redis
 
+import com.flab.inqueue.TestContainer
 import com.flab.inqueue.createEventRequest
 import com.flab.inqueue.domain.queue.entity.Work
-import com.flab.inqueue.support.EmbeddedRedisConfig
-import org.assertj.core.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
+import com.flab.inqueue.support.RedisConfigTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
@@ -12,69 +13,75 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest
 import org.springframework.context.annotation.Import
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.test.context.ActiveProfiles
 import java.util.*
 
 
 @DataRedisTest
-@ActiveProfiles("test")
-@Import(EmbeddedRedisConfig::class)
+@Import(RedisConfigTest::class)
 class WaitWorkRedis @Autowired constructor(
     private val redisTemplate: RedisTemplate<String, Any>,
-) {
+) : TestContainer() {
     private val logger = LoggerFactory.getLogger(WaitWorkRedis::class.java)
-
-
+    private val zSetOperations = redisTemplate.opsForZSet();
 
     @Test
-    fun `등록`() {
+    @DisplayName("레디스 실행 테스트")
+    fun redisRunningTest() {
+        assertTrue(redisContainer.isRunning);
+    }
+
+    @Test
+    @DisplayName("등록")
+    fun add() {
         //given
         val event = createEventRequest().toEntity()
-        val work = Work(event.eventId, UUID.randomUUID().toString().take(10))
-        redisTemplate.opsForZSet().add(event.eventId, work, System.nanoTime().toDouble())
+        val work = Work(event.eventId, UUID.randomUUID().toString())
+        zSetOperations.add(event.eventId, work, System.nanoTime().toDouble())
         //when
-        val range = redisTemplate.opsForZSet().range(event.eventId, 0, -1)
+        val range = zSetOperations.range(event.eventId, 0, -1)
         //then
         assertThat(range?.size).isEqualTo(1)
     }
 
     @Test
-    fun `key의 전체 사이즈(갯수) 조회`() {
+    @DisplayName("key의 전체 사이즈(갯수) 조회")
+    fun getKeySize() {
         //given
         val event = createEventRequest().toEntity()
-        redisTemplate.opsForZSet().add(
-            event.eventId, Work(event.eventId, UUID.randomUUID().toString().take(10)), System.nanoTime().toDouble()
+        zSetOperations.add(
+            event.eventId, Work(event.eventId, UUID.randomUUID().toString()), System.nanoTime().toDouble()
         )
-        redisTemplate.opsForZSet().add(
-            event.eventId, Work(event.eventId, UUID.randomUUID().toString().take(10)), System.nanoTime().toDouble()
+        zSetOperations.add(
+            event.eventId, Work(event.eventId, UUID.randomUUID().toString()), System.nanoTime().toDouble()
         )
-        redisTemplate.opsForZSet().add(
-            event.eventId, Work(event.eventId, UUID.randomUUID().toString().take(10)), System.nanoTime().toDouble()
+        zSetOperations.add(
+            event.eventId, Work(event.eventId, UUID.randomUUID().toString()), System.nanoTime().toDouble()
         )
         //when
-        val long = redisTemplate.opsForZSet().size(event.eventId)
+        val long = zSetOperations.size(event.eventId)
         logger.info("{}", long)
         //then
         assertThat(long).isEqualTo(3)
     }
 
     @Test
-    fun `key의 rank 조회`() {
+    @DisplayName("key의 rank 조회")
+    fun getRanksOfKey() {
         //given
         val event = createEventRequest().toEntity()
-        val work1 = Work(event.eventId, UUID.randomUUID().toString().take(10))
-        val work2 = Work(event.eventId, UUID.randomUUID().toString().take(10))
-        val work3 = Work(event.eventId, UUID.randomUUID().toString().take(10))
-        redisTemplate.opsForZSet().add(event.eventId, work1, System.nanoTime().toDouble())
-        redisTemplate.opsForZSet().add(event.eventId, work2, System.nanoTime().toDouble())
-        redisTemplate.opsForZSet().add(event.eventId, work3, System.nanoTime().toDouble())
+        val work1 = Work(event.eventId, UUID.randomUUID().toString())
+        val work2 = Work(event.eventId, UUID.randomUUID().toString())
+        val work3 = Work(event.eventId, UUID.randomUUID().toString())
+        zSetOperations.add(event.eventId, work1, System.nanoTime().toDouble())
+        zSetOperations.add(event.eventId, work2, System.nanoTime().toDouble())
+        zSetOperations.add(event.eventId, work3, System.nanoTime().toDouble())
 
 
         //when
-        val totalSize = redisTemplate.opsForZSet().size(event.eventId)
-        val rank1 = redisTemplate.opsForZSet().rank(event.eventId, work1)
-        val rank2 = redisTemplate.opsForZSet().rank(event.eventId, work2)
-        val rank3 = redisTemplate.opsForZSet().rank(event.eventId, work3)
+        val totalSize = zSetOperations.size(event.eventId)
+        val rank1 = zSetOperations.rank(event.eventId, work1)
+        val rank2 = zSetOperations.rank(event.eventId, work2)
+        val rank3 = zSetOperations.rank(event.eventId, work3)
 
         //then
         assertThat(rank1).isEqualTo(0)
@@ -83,20 +90,21 @@ class WaitWorkRedis @Autowired constructor(
 
 
     @Test
-    fun `key의 rank 삭제 `() {
+    @DisplayName("key 삭제")
+    fun deleteKey() {
         //given
         val event = createEventRequest().toEntity()
-        val work1 = Work(event.eventId, UUID.randomUUID().toString().take(10))
-        val work2 = Work(event.eventId, UUID.randomUUID().toString().take(10))
-        val work3 = Work(event.eventId, UUID.randomUUID().toString().take(10))
-        redisTemplate.opsForZSet().add(event.eventId, work1, System.nanoTime().toDouble())
-        redisTemplate.opsForZSet().add(event.eventId, work2, System.nanoTime().toDouble())
-        redisTemplate.opsForZSet().add(event.eventId, work3, System.nanoTime().toDouble())
+        val work1 = Work(event.eventId, UUID.randomUUID().toString())
+        val work2 = Work(event.eventId, UUID.randomUUID().toString())
+        val work3 = Work(event.eventId, UUID.randomUUID().toString())
+        zSetOperations.add(event.eventId, work1, System.nanoTime().toDouble())
+        zSetOperations.add(event.eventId, work2, System.nanoTime().toDouble())
+        zSetOperations.add(event.eventId, work3, System.nanoTime().toDouble())
 
 
         //when
-        val remove = redisTemplate.opsForZSet().remove(event.eventId, work1)
-        val totalSize = redisTemplate.opsForZSet().size(event.eventId)
+        val remove = zSetOperations.remove(event.eventId, work1)
+        val totalSize = zSetOperations.size(event.eventId)
 
         //then
         assertThat(remove).isNotNull
@@ -104,35 +112,37 @@ class WaitWorkRedis @Autowired constructor(
     }
 
     @Test
-    fun `key의 범위 조회`() {
+    @DisplayName("key의 범위 조회")
+    fun getRetrieveFromRange() {
         //given
         val event = createEventRequest().toEntity()
         repeat(100) {
-            redisTemplate.opsForZSet().add(
-                event.eventId, Work(event.eventId, UUID.randomUUID().toString().take(10)), System.nanoTime().toDouble()
+            zSetOperations.add(
+                event.eventId, Work(event.eventId, UUID.randomUUID().toString()), System.nanoTime().toDouble()
             )
         }
 
         //when
-        val range = redisTemplate.opsForZSet().range(event.eventId, 0, 9)
+        val range = zSetOperations.range(event.eventId, 0, 9)
 
         //then
         assertThat(range?.size).isEqualTo(10)
     }
 
     @Test
-    fun `key의 범위 삭제`() {
+    @DisplayName("key의 범위 삭제")
+    fun deleteFromRangeWithKey() {
         //given
         val event = createEventRequest().toEntity()
         repeat(100) {
-            redisTemplate.opsForZSet().add(
-                event.eventId, Work(event.eventId, UUID.randomUUID().toString().take(10)), System.nanoTime().toDouble()
+            zSetOperations.add(
+                event.eventId, Work(event.eventId, UUID.randomUUID().toString()), System.nanoTime().toDouble()
             )
         }
 
         //when
-        val range = redisTemplate.opsForZSet().removeRange(event.eventId, 0, 9)
-        val size = redisTemplate.opsForZSet().size(event.eventId)
+        val range = zSetOperations.removeRange(event.eventId, 0, 9)
+        val size = zSetOperations.size(event.eventId)
         //then
         assertThat(size).isEqualTo(90)
     }
