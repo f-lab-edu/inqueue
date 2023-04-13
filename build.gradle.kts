991cf6af116cd23b32a0a8463038d9505b3a59c7
JKjiwon <jwkim.oa@gmail.com>
2023-04-13 15:48:50 +0900

    build : jwt 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
 
     /* jwt */
     implementation ("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
