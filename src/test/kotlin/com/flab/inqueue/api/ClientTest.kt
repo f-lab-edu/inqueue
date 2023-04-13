5e894007e2e5fc320e3a3b5fb3ae1f7d3c381df4
elyo9381 <won4413@daum.net>
2023-04-13 17:56:23 +0900

    test : 행사 생성 api atdd 및 restdoc  스니펫 수정

--- src/test/kotlin/com/flab/inqueue/api/ClientTest.kt
+++ src/test/kotlin/com/flab/inqueue/api/ClientTest.kt
@@ -2,6 +2,7 @@ package com.flab.inqueue.api
 
 import com.flab.inqueue.AcceptanceTest
 import com.flab.inqueue.REST_DOCS_DOCUMENT_IDENTIFIER
+import com.flab.inqueue.domain.event.dto.EventInformation
 import com.flab.inqueue.domain.event.dto.EventRequest
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
@@ -11,10 +12,12 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.restdocs.headers.HeaderDocumentation.headerWithName
 import org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders
+import org.springframework.restdocs.operation.preprocess.Preprocessors
 import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.restdocs.payload.PayloadDocumentation.*
 import org.springframework.restdocs.restassured.RestAssuredRestDocumentation.document
 import org.springframework.restdocs.restassured.RestDocumentationFilter
+import org.springframework.restdocs.snippet.Attributes.*
 import org.springframework.restdocs.snippet.Snippet
 import java.time.LocalDateTime
 
@@ -29,8 +32,14 @@ class ClientTest : AcceptanceTest() {
             LocalDateTime.now(),
             1L,
             1L,
-            null,
-            null
+            EventInformation("testEvent",
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                "test description",
+                "test place",
+                100L,
+                "TEST CONCERT"),
+            "https://test"
         )
 
         val response = given.log().all()
@@ -46,18 +55,22 @@ class ClientTest : AcceptanceTest() {
 
         val body = response.body().jsonPath()
 
-        assertThat(body.get<String>("eventId")).isEqualTo("eventId")
+        assertThat(body.get<String>("eventId")).isNotNull
+        assertThat(body.get<String>("eventId")).isNotEmpty
     }
 
 
 }
 
 object CreateEventDocument {
+
     val FILTER: RestDocumentationFilter = document(
         REST_DOCS_DOCUMENT_IDENTIFIER,
+        Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+        Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
         headerFiledSnippet(),
         requestFieldsSnippet(),
-        responseFieldsSnippet()
+        responseFieldsSnippet(),
     )
 
     private fun headerFiledSnippet() : Snippet {
@@ -69,18 +82,19 @@ object CreateEventDocument {
 
     private fun requestFieldsSnippet(): Snippet {
         return requestFields(
-            fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
-            fieldWithPath("description").type(JsonFieldType.STRING).description("설명"),
-            fieldWithPath("place").type(JsonFieldType.STRING).description("장소"),
-            fieldWithPath("startTime").type(JsonFieldType.ARRAY).description("행사 시작 시간"),
-            fieldWithPath("endTime").type(JsonFieldType.ARRAY).description("행사 마침 시간"),
-            fieldWithPath("personnel").type(JsonFieldType.NUMBER).description("인원"),
+            fieldWithPath("eventId").type(JsonFieldType.STRING).description("이벤트 식별자").optional(),
+            fieldWithPath("eventInformation.name").type(JsonFieldType.STRING).description("이름").optional(),
+            fieldWithPath("eventInformation.description").type(JsonFieldType.STRING).description("설명").optional(),
+            fieldWithPath("eventInformation.place").type(JsonFieldType.STRING).description("장소").optional(),
+            fieldWithPath("eventInformation.startTime").type(JsonFieldType.ARRAY).description("행사 시작 시간").optional(),
+            fieldWithPath("eventInformation.endTime").type(JsonFieldType.ARRAY).description("행사 마침 시간").optional(),
+            fieldWithPath("eventInformation.personnel").type(JsonFieldType.NUMBER).description("인원").optional(),
             fieldWithPath("waitQueueStartTime").type(JsonFieldType.ARRAY).description("대기 큐 시작 시간"),
             fieldWithPath("waitQueueEndTime").type(JsonFieldType.ARRAY).description("대기 큐 마침 시간"),
-            fieldWithPath("type").type(JsonFieldType.STRING).description("행사 종류"),
+            fieldWithPath("eventInformation.type").type(JsonFieldType.STRING).description("행사 종류").optional(),
             fieldWithPath("jobQueueSize").type(JsonFieldType.NUMBER).description("작업 큐 크기"),
             fieldWithPath("jobQueueLimitTime").type(JsonFieldType.NUMBER).description("작업 큐 제한 시간"),
-            fieldWithPath("redirectUrl").type(JsonFieldType.STRING).description("redirectUrl"),
+            fieldWithPath("redirectUrl").type(JsonFieldType.STRING).description("redirectUrl").optional(),
         )
     }
 
