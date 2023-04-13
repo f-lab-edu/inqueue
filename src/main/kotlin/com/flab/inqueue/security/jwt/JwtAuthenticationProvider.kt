87b8d5d9078721a1868abad4d80c5ed168d3d6e7
JKjiwon <jwkim.oa@gmail.com>
2023-04-13 16:38:49 +0900

    feat : JWT 인증 Role 추가

--- src/main/kotlin/com/flab/inqueue/security/jwt/JwtAuthenticationProvider.kt
+++ src/main/kotlin/com/flab/inqueue/security/jwt/JwtAuthenticationProvider.kt
@@ -4,12 +4,17 @@ import com.flab.inqueue.security.jwt.utils.JwtUtils
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.stereotype.Component
 
 @Component
 class JwtAuthenticationProvider(
     private val jwtUtils: JwtUtils,
 ) : AuthenticationProvider {
+
+    companion object {
+        private const val DEFAULT_USER_ROLE = "ROLE_USER"
+    }
 
     override fun authenticate(authentication: Authentication?): Authentication {
         val jwtAuthentication = authentication as JwtAuthenticationToken
@@ -25,7 +30,11 @@ class JwtAuthenticationProvider(
         // TODO: 고객 검증 구현 - RDB 에 고객이 있는지 확인
         // TODO: 대기열 유저 검증 구현 - Redis UserList 에서 해당 유저가 있는지 확인
 
-        return JwtAuthenticationToken.authenticatedToken(verifyResponse.clientId!!, verifyResponse.userId!!)
+        return JwtAuthenticationToken.authenticatedToken(
+            clientId = verifyResponse.clientId!!,
+            userId = verifyResponse.userId!!,
+            authorities = mutableListOf(SimpleGrantedAuthority(DEFAULT_USER_ROLE))
+        )
     }
 
     override fun supports(authentication: Class<*>): Boolean {
