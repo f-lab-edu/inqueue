8585e8805e26c662aeed71ca2a4252ea0dce51b6
elyo9381 <won4413@daum.net>
2023-04-13 22:45:56 +0900

    fix : 대기열 서비스 등록 및 조회 서비스 추가

--- src/main/kotlin/com/flab/inqueue/domain/queue/repository/QueueRedisRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.flab.inqueue.domain.queue.repository
-
-import com.flab.inqueue.domain.queue.entity.Job
-import org.springframework.data.repository.CrudRepository
-
-interface QueueRedisRepository : CrudRepository<Job, String>
--- src/main/kotlin/com/flab/inqueue/domain/queue/service/QueueService.kt
+++ src/main/kotlin/com/flab/inqueue/domain/queue/service/QueueService.kt
@@ -1,30 +1,30 @@
 package com.flab.inqueue.domain.queue.service
 
 import com.flab.inqueue.domain.queue.entity.Job
-import org.springframework.data.redis.core.RedisTemplate
+import com.flab.inqueue.domain.queue.repository.QueueRedisPository
 import org.springframework.stereotype.Service
 
 @Service
 class QueueService(
-    private val redisTemplate: RedisTemplate<String, Any>,
+    private val queueRedisPository: QueueRedisPository
 ) {
 
-    fun register(work: Job): Long? {
-        redisTemplate.opsForZSet().add(work.eventId, work, System.nanoTime().toDouble())
-        return getRedisSize(work.eventId)
+    fun register(job: Job) {
+        return queueRedisPository.register(job)
     }
 
-    fun registerAll(work: Job): Long? {
-        redisTemplate.opsForZSet().add(work.eventId, work, System.nanoTime().toDouble())
-        return getRedisSize(work.eventId)
+    fun size(key: String): Long? {
+        return queueRedisPository.size(key)
     }
 
-    fun getRedisSize(key: String): Long? {
-        return redisTemplate.opsForZSet().size(key);
+    fun rank(job: Job): Long? {
+        return queueRedisPository.rank(job)
     }
-
-    fun getRank(job: Job): Long? {
-        return redisTemplate.opsForZSet().rank(job.eventId, job)
+    fun range(key: String, start: Long, end: Long): MutableSet<Job>? {
+        return queueRedisPository.range(key,start,end)
+    }
+    fun deleteRange(key: String, start: Long, end: Long): Long? {
+        return queueRedisPository.deleteRange(key,start,end)
     }
 
 }
