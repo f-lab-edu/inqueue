8cf65530071a03a54af02f8cac0a675495641778
JKjiwon <jwkim.oa@gmail.com>
2023-04-18 19:05:10 +0900

    refactor : ErrorResponse 의 static factory method 삭제
    - ErrorResponse 는 표현 계층이므로 현재 구조는 exception 처리에 대한 유연성이 떨어진다.
    - exception handler 에서 ErrorResponse 를 만들어 전달하는 방향으로 변경

--- src/main/kotlin/com/flab/inqueue/application/common/ErrorResponse.kt
+++ src/main/kotlin/com/flab/inqueue/application/common/ErrorResponse.kt
@@ -1,37 +1,10 @@
 package com.flab.inqueue.application.common
 
-import jakarta.servlet.http.HttpServletRequest
-import org.springframework.http.HttpStatus
 import java.time.LocalDateTime
 
 data class ErrorResponse(
-    val timestamp: LocalDateTime,
+    val timestamp: LocalDateTime = LocalDateTime.now(),
     val error: String,
     val status: Int,
     val path: String
-) {
-    companion object {
-        private const val DEFAULT_UNAUTHORIZED_ERROR_MESSAGE = "Unauthorized"
-        private const val DEFAULT_FORBIDDEN_ERROR_MESSAGE = "Forbidden"
-
-        @JvmStatic
-        fun unAuthorized(request: HttpServletRequest): ErrorResponse {
-            return ErrorResponse(
-                timestamp = LocalDateTime.now(),
-                error = DEFAULT_UNAUTHORIZED_ERROR_MESSAGE,
-                status = HttpStatus.UNAUTHORIZED.value(),
-                path = request.requestURI
-            )
-        }
-
-        @JvmStatic
-        fun forbidden(request: HttpServletRequest): ErrorResponse {
-            return ErrorResponse(
-                timestamp = LocalDateTime.now(),
-                error = DEFAULT_FORBIDDEN_ERROR_MESSAGE,
-                status = HttpStatus.FORBIDDEN.value(),
-                path = request.requestURI
-            )
-        }
-    }
-}
+)
--- src/main/kotlin/com/flab/inqueue/security/CustomAuthenticationEntryPoint.kt
+++ src/main/kotlin/com/flab/inqueue/security/CustomAuthenticationEntryPoint.kt
@@ -13,15 +13,24 @@ import org.springframework.stereotype.Component
 class CustomAuthenticationEntryPoint(
     private val objectMapper: ObjectMapper
 ) : AuthenticationEntryPoint {
+
+    companion object {
+        private const val DEFAULT_UNAUTHORIZED_ERROR_MESSAGE = "Unauthorized"
+    }
+
     override fun commence(
         request: HttpServletRequest,
         response: HttpServletResponse,
         authException: AuthenticationException
     ) {
         response.status = HttpServletResponse.SC_UNAUTHORIZED
-        val jsonResponse = ErrorResponse.unAuthorized(request)
-
         response.contentType = MediaType.APPLICATION_JSON_VALUE
+
+        val jsonResponse = ErrorResponse(
+            error = DEFAULT_UNAUTHORIZED_ERROR_MESSAGE,
+            status = HttpServletResponse.SC_UNAUTHORIZED,
+            path = request.requestURI
+        )
         response.writer.write(objectMapper.writeValueAsString(jsonResponse))
     }
 }
