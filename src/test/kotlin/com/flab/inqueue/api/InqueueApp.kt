42380a56742423872970af4710a0836f354c1170
JKjiwon <jwkim.oa@gmail.com>
2023-04-19 19:36:53 +0900

    test: 스프링 시큐리티 테스트 격리 비활성화
    - 오류 해결 후 다시 적용

--- src/test/kotlin/com/flab/inqueue/api/InqueueApp.kt
+++
@@ -1,7 +0,0 @@
-package com.flab.inqueue.api
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication
-class InqueueApp {
-}
--- src/test/kotlin/com/flab/inqueue/security/WebSecurityApp.kt
+++ src/test/kotlin/com/flab/inqueue/security/WebSecurityApp.kt
@@ -1,18 +1,18 @@
-package com.flab.inqueue.security
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-
-@ConfigurationPropertiesScan("com.flab.inqueue")
-@SpringBootApplication(
-    scanBasePackages = [
-        "com.flab.inqueue.security",
-        "com.flab.inqueue.domain.member",
-    ]
-)
-@EnableJpaRepositories("com.flab.inqueue.domain.member")
-@EntityScan("com.flab.inqueue.domain.member")
-class WebSecurityApp {
-}
+//package com.flab.inqueue.security
+//
+//import org.springframework.boot.autoconfigure.SpringBootApplication
+//import org.springframework.boot.autoconfigure.domain.EntityScan
+//import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+//
+//@ConfigurationPropertiesScan("com.flab.inqueue")
+//@SpringBootApplication(
+//    scanBasePackages = [
+//        "com.flab.inqueue.security",
+//        "com.flab.inqueue.domain.member"
+//    ]
+//)
+//@EnableJpaRepositories("com.flab.inqueue.domain.member")
+//@EntityScan("com.flab.inqueue.domain.member")
+//class WebSecurityApp {
+//}
