9a9d5a15e42301be54d6be711b9f6bd3a3555bcf
elyo9381 <won4413@daum.net>
2023-04-20 16:05:08 +0900

    fix : Job 객체 생성시 초기화 진행 및 상태 추가

--- src/main/kotlin/com/flab/inqueue/domain/queue/entity/Job.kt
+++ src/main/kotlin/com/flab/inqueue/domain/queue/entity/Job.kt
@@ -1,20 +1,13 @@
 package com.flab.inqueue.domain.queue.entity
 
 class Job(
-    val eventId: String,
+    var eventId: String,
     val userId: String,
+    var status : JobStatus = JobStatus.WAIT
 ) {
-
-    companion object{
-        fun from(job : Job): Job {
-            return Job(job.eventId,job.userId)
-        }
-        fun enterJobFrom(job : Job): Job {
-            val stringBuffer = StringBuilder()
-            stringBuffer.append(job.eventId)
-            stringBuffer.append(":JOB_QUEUE")
-            return Job(stringBuffer.toString(),job.userId)
-        }
+    init {
+        val stringBuffer = StringBuilder(status.prefix)
+        stringBuffer.append(eventId)
+        this.eventId = stringBuffer.toString()
     }
-
 }
