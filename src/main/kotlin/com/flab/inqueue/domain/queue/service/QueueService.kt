b93dc186a7e8a06cbf7c8067036ec4f415497a92
elyo9381 <won4413@daum.net>
2023-04-20 16:06:25 +0900

    fix : 각 service의 책임에 따른 기능 이동

--- src/main/kotlin/com/flab/inqueue/domain/queue/service/QueueService.kt
+++ src/main/kotlin/com/flab/inqueue/domain/queue/service/QueueService.kt
@@ -1,25 +1,37 @@
 package com.flab.inqueue.domain.queue.service
 
+import com.flab.inqueue.domain.queue.dto.QueueInfo
+import com.flab.inqueue.domain.queue.dto.QueueResponse
 import com.flab.inqueue.domain.queue.entity.Job
+import com.flab.inqueue.domain.queue.entity.JobStatus
 import com.flab.inqueue.domain.queue.repository.QueueRedisPository
 import org.springframework.stereotype.Service
+import java.time.LocalTime
 
 @Service
 class QueueService(
     private val queueRedisPository: QueueRedisPository
 ) {
 
-    fun register(job: Job) {
-        return queueRedisPository.register(job)
+    fun register(job: Job): QueueResponse {
+        queueRedisPository.register(job)
+        if( job.status == JobStatus.ENTER){
+            return QueueResponse(job.status)
+        }
+        return retrieve(job)
     }
 
     fun size(key: String): Long? {
         return queueRedisPository.size(key)
     }
 
-    fun rank(job: Job): Long? {
-        return queueRedisPository.rank(job)
+    fun retrieve(job: Job): QueueResponse {
+        val rank = (queueRedisPository.rank(job) ?: 0) + 1
+        val waitSecond = rank * 10
+        val waitTime = LocalTime.now().plusSeconds(waitSecond)
+        return QueueResponse( JobStatus.WAIT, QueueInfo(waitTime, rank.toInt()))
     }
+
     fun range(key: String, start: Long, end: Long): MutableSet<Job>? {
         return queueRedisPository.range(key,start,end)
     }
