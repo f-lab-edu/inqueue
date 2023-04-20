01319697ad2073fe11eef6281549ecfab4767228
elyo9381 <won4413@daum.net>
2023-04-20 16:06:44 +0900

    test : queueServiceTest 추가

---
+++ src/test/kotlin/com/flab/inqueue/domain/queue/service/QueueServiceTest.kt
@@ -0,0 +1,70 @@
+package com.flab.inqueue.domain.queue.service
+
+import com.flab.inqueue.domain.queue.entity.Job
+import com.flab.inqueue.domain.queue.entity.JobStatus
+import com.flab.inqueue.domain.queue.repository.QueueRedisPository
+import com.flab.inqueue.support.UnitTest
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import java.time.LocalTime
+import java.util.*
+
+@UnitTest
+class QueueServiceTest {
+
+    @MockK
+    lateinit var queueRedisPository: QueueRedisPository
+
+    @InjectMockKs
+    lateinit var queueService: QueueService
+
+
+    @DisplayName("대기열 및 작업열 진입")
+    @ParameterizedTest(name = "{index} ==> ''{0}'' test")
+    @EnumSource(JobStatus::class)
+    fun enterJobQueue(jobStatus: JobStatus) {
+        //given
+        val eventId = "TestEventId"
+        val userId = UUID.randomUUID().toString()
+
+        //given
+        every { queueRedisPository.register(any()) } returns Unit
+        every { queueRedisPository.rank(any()) } returns 1
+
+        val queueResponse = queueService.register(Job(eventId, userId, jobStatus))
+
+        //then
+        assertThat(queueResponse.status).isEqualTo(jobStatus)
+
+        if (queueResponse.status == JobStatus.ENTER) {
+            assertThat(queueResponse.expectedInfo).isNull()
+        } else {
+            assertThat(queueResponse.expectedInfo?.time).isAfter(LocalTime.now())
+            assertThat(queueResponse.expectedInfo?.order).isGreaterThan(0)
+        }
+    }
+
+    @Test
+    @DisplayName("대기열 조회")
+    fun enterWaitQueue() {
+        //given
+        val eventId = "TestEventId"
+        val userId = UUID.randomUUID().toString()
+
+        every { queueRedisPository.rank(any()) } returns 1
+
+        //given
+        val queueResponse = queueService.retrieve(Job(eventId, userId))
+
+        //then
+        assertThat(queueResponse.status).isEqualTo(JobStatus.WAIT)
+        assertThat(queueResponse.expectedInfo?.time).isAfter(LocalTime.now())
+        assertThat(queueResponse.expectedInfo?.order).isGreaterThan(0)
+    }
+}
