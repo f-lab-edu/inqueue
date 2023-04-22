a01bf0498f2a47e0f2dd27a1e866381a8964d4e5
elyo9381 <won4413@daum.net>
2023-04-22 17:43:11 +0900

    fix : jobQueue 자료구조 set으로 변경으로 인한 로직 변경

--- src/main/kotlin/com/flab/inqueue/domain/queue/service/JobService.kt
+++ src/main/kotlin/com/flab/inqueue/domain/queue/service/JobService.kt
@@ -1,5 +1,6 @@
 package com.flab.inqueue.domain.queue.service
 
+import com.flab.inqueue.domain.event.entity.Event
 import com.flab.inqueue.domain.event.repository.EventRepository
 import com.flab.inqueue.domain.queue.dto.QueueResponse
 import com.flab.inqueue.domain.queue.entity.Job
@@ -13,21 +14,30 @@ class JobService(
 ) {
 
     fun enter(eventId: String, userId: String): QueueResponse {
-        val job = if (isEnterJob(eventId)) Job(eventId, userId, JobStatus.ENTER)
-        else Job(eventId, userId, JobStatus.WAIT)
-        return queueService.register(job)
+        val event = findEvent(eventId)
+
+        val job =
+            if (isEnterJob(event)) Job(eventId, userId, JobStatus.ENTER, event.period.convertInstant())
+            else Job(eventId, userId, JobStatus.WAIT, event.period.convertInstant())
+
+        return queueService.waitQueueRegister(job)
     }
+
 
     fun retrieve(eventId: String, userId: String): QueueResponse {
         val waitJob = Job(eventId, userId, JobStatus.WAIT)
-        return queueService.retrieve(waitJob)
+        return queueService.waitQueueRetrieve(waitJob)
     }
 
-    private fun isEnterJob(eventId: String): Boolean {
-        val event = eventRepository.findByEventId(eventId) ?: throw NoSuchElementException("행사를 찾을 수 없습니다. $eventId")
+    private fun findEvent(eventId: String): Event {
+        return eventRepository.findByEventId(eventId) ?: throw NoSuchElementException("행사를 찾을 수 없습니다. $eventId")
+    }
 
-        val enterQueueSize = queueService.size(JobStatus.ENTER.makeRedisKey(eventId)) ?: 0
-        val waitQueueSize = queueService.size(JobStatus.WAIT.makeRedisKey(eventId)) ?: 0
+    private fun isEnterJob(event: Event): Boolean {
+
+        // 에서 조회를 해야함
+        val enterQueueSize = queueService.size(JobStatus.ENTER.makeRedisKey(event.eventId)) ?: 0
+        val waitQueueSize = queueService.size(JobStatus.WAIT.makeRedisKey(event.eventId)) ?: 0
 
         if (waitQueueSize > 0) return false
         if (enterQueueSize > event.jobQueueLimitTime) return false
--- src/main/kotlin/com/flab/inqueue/domain/queue/service/QueueService.kt
+++ src/main/kotlin/com/flab/inqueue/domain/queue/service/QueueService.kt
@@ -4,37 +4,46 @@ import com.flab.inqueue.domain.queue.dto.QueueInfo
 import com.flab.inqueue.domain.queue.dto.QueueResponse
 import com.flab.inqueue.domain.queue.entity.Job
 import com.flab.inqueue.domain.queue.entity.JobStatus
+import com.flab.inqueue.domain.queue.repository.JobQueueRedisRepository
 import com.flab.inqueue.domain.queue.repository.WaitQueueRedisRepository
 import org.springframework.stereotype.Service
 
 @Service
 class QueueService(
-    private val waitQueueRedisRepository: WaitQueueRedisRepository
+    private val waitQueueRedisRepository: WaitQueueRedisRepository,
+    private val jobQueueRedisRepository: JobQueueRedisRepository,
 ) {
 
-    fun register(job: Job): QueueResponse {
-        waitQueueRedisRepository.register(job)
-        if( job.status == JobStatus.ENTER){
+    fun waitQueueRegister(job: Job): QueueResponse {
+        jobQueueRedisRepository.register(job)
+        if (job.status == JobStatus.ENTER) {
             return QueueResponse(job.status)
         }
-        return retrieve(job)
+        waitQueueRedisRepository.register(job)
+        return waitQueueRetrieve(job)
+    }
+
+    fun waitQueueRetrieve(job: Job): QueueResponse {
+        if (!isMember(job)) return QueueResponse(JobStatus.TIMEOUT)
+        val rank = (waitQueueRedisRepository.rank(job) ?: 0) + 1
+        val waitSecond = rank * 10
+        return QueueResponse(JobStatus.WAIT, QueueInfo(waitSecond, rank.toInt()))
     }
 
     fun size(key: String): Long? {
-        return waitQueueRedisRepository.size(key)
+        return jobQueueRedisRepository.size(key)
     }
 
-    fun retrieve(job: Job): QueueResponse {
-        val rank = (waitQueueRedisRepository.rank(job) ?: 0) + 1
-        val waitSecond = rank * 10
-        return QueueResponse( JobStatus.WAIT, QueueInfo(waitSecond, rank.toInt()))
+    fun isMember(job: Job): Boolean {
+        return jobQueueRedisRepository.isMember(job)
     }
 
     fun range(key: String, start: Long, end: Long): MutableSet<Job>? {
-        return waitQueueRedisRepository.range(key,start,end)
+        return waitQueueRedisRepository.range(key, start, end)
     }
+
     fun deleteRange(key: String, start: Long, end: Long): Long? {
-        return waitQueueRedisRepository.deleteRange(key,start,end)
+        return waitQueueRedisRepository.deleteRange(key, start, end)
     }
 
 }
