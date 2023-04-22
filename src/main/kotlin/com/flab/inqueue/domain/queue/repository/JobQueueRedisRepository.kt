12fd719211008e3117b4faacf879a797983dbd8f
elyo9381 <won4413@daum.net>
2023-04-22 17:41:11 +0900

    fix : redis의 SET 자료구조 통해서 TTL 등록 및 redis 로직 수정

--- src/main/kotlin/com/flab/inqueue/domain/queue/repository/JobQueueRedisRepository.kt
+++ src/main/kotlin/com/flab/inqueue/domain/queue/repository/JobQueueRedisRepository.kt
@@ -3,24 +3,36 @@ package com.flab.inqueue.domain.queue.repository
 import com.flab.inqueue.domain.queue.entity.Job
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Repository
+import java.util.concurrent.TimeUnit
 
 @Repository
 class JobQueueRedisRepository(
-    private val jobRedisTemplate: RedisTemplate<String, Job>,
+    private val redisTemplate: RedisTemplate<String, String>,
 ) {
+
     fun register(job: Job) {
-        jobRedisTemplate.opsForSet().add(job.redisKey(), job)
+        redisTemplate.opsForSet().add(job.redisKey(), job.redisValue() )
+        job.redisKeySecTTL?.let { redisTemplate.expireAt(job.redisKey(), it) }
+        redisTemplate.opsForSet().add(job.redisValue() ,job.redisValue() )
+        redisTemplate.expire(job.redisValue(), 1L, TimeUnit.SECONDS)
     }
 
-    fun remove(job: Job): Long? {
-        return jobRedisTemplate.opsForZSet().remove(job.redisKey(), job)
+    fun remove(job: Job): Boolean {
+        redisTemplate.opsForSet().remove(job.redisValue(), job.redisValue()) ?: return false
+        redisTemplate.opsForSet().remove(job.redisKey(), job.redisValue()) ?: return false
+        return true
     }
-
     fun size(key: String): Long? {
-        return jobRedisTemplate.opsForZSet().size(key)
+        return redisTemplate.opsForSet().size(key);
     }
 
-    fun isMember(job: Job): Boolean? {
-        return jobRedisTemplate.opsForSet().isMember(job.redisKey(), job)
+    fun isMember(job: Job): Boolean {
+        val isRedisValue = redisTemplate.opsForSet().isMember(job.redisValue(), job.redisValue())
+        if(isRedisValue == true) {
+            return true
+        } else {
+            redisTemplate.opsForSet().remove(job.redisKey(), job.redisValue())
+            return false;
+        }
     }
 }
