8949b621535b569fc9b2cd25a866ffd94ffd2684
elyo9381 <won4413@daum.net>
2023-04-22 17:46:05 +0900

    test refactor : redis 자료구조 변경으로 인한 테스트 코드 변경

--- src/test/kotlin/com/flab/inqueue/support/RedisConfigTest.kt
+++ src/test/kotlin/com/flab/inqueue/support/RedisConfigTest.kt
@@ -3,19 +3,26 @@ package com.flab.inqueue.support
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.flab.inqueue.domain.queue.entity.Job
 import com.flab.inqueue.infra.property.RedisProperty
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.context.event.EventListener
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisKeyExpiredEvent
+import org.springframework.data.redis.core.RedisKeyValueAdapter
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
+import org.springframework.stereotype.Component
 
 
 @TestConfiguration
 @EnableConfigurationProperties(value = [RedisProperty::class])
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 class RedisConfigTest(
     private val redisProperty: RedisProperty,
 ) {
@@ -25,12 +32,28 @@ class RedisConfigTest(
         return LettuceConnectionFactory(RedisStandaloneConfiguration(redisProperty.host, redisProperty.port))
     }
 
-    @Bean
-    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
-        val redisTemplate = RedisTemplate<String, Any>()
+    @Bean(name = ["jobRedisTemplate"])
+    fun jobRedisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, Job> {
+        val redisTemplate = RedisTemplate<String, Job>()
         redisTemplate.keySerializer = StringRedisSerializer()
         redisTemplate.valueSerializer = Jackson2JsonRedisSerializer(jacksonObjectMapper(), Job::class.java)
         redisTemplate.setConnectionFactory(connectionFactory)
         return redisTemplate
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<*, *> {
+        val redisTemplate: RedisTemplate<*, *> = RedisTemplate<Any, Any>()
+        redisTemplate.setConnectionFactory(redisConnectionFactory()!!)
+        return redisTemplate
+    }
+
+    @Component
+    class SessionExpiredEventListener {
+        private val log = LoggerFactory.getLogger(SessionExpiredEventListener::class.java)
+        @EventListener
+        fun handleRedisKeyExpiredEvent(event: RedisKeyExpiredEvent<String>) {
+            log.info("JobUser key={} has expired", String(event.id))
+        }
     }
 }
