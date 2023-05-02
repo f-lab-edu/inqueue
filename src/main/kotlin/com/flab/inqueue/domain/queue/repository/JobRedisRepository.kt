a491244e7d23791e4c8dfe212ed108aeb5b37306
JKjiwon <jwkim.oa@gmail.com>
2023-05-02 11:29:59 +0900

    refactor : 대기열, 작업열 RedisRepository 분리

--- src/main/kotlin/com/flab/inqueue/domain/queue/repository/JobRedisRepository.kt
+++ src/main/kotlin/com/flab/inqueue/domain/queue/repository/JobRedisRepository.kt
@@ -8,35 +8,35 @@ import java.util.concurrent.TimeUnit
 
 @Repository
 class JobRedisRepository(
-    private val redisTemplate: RedisTemplate<String, String>,
+    private val jobRedisTemplate: RedisTemplate<String, String>,
+    private val userRedisTemplate: RedisTemplate<String, String>,
 ) {
 
     @Transactional
     fun register(job: Job) {
-        redisTemplate.opsForSet().add(job.redisKey(), job.redisValue())
-        redisTemplate.opsForValue().set(job.redisValue(), job.redisValue(), job.workingTimeSec, TimeUnit.SECONDS)
+        jobRedisTemplate.opsForSet().add(job.redisKey(), job.redisValue())
+        userRedisTemplate.opsForValue().set(job.redisValue(), job.redisValue(), job.workingTimeSec, TimeUnit.SECONDS)
     }
 
     @Transactional
     fun remove(job: Job): Boolean {
-        redisTemplate.opsForSet().remove(job.redisKey(), job.redisValue())
-        redisTemplate.opsForValue().getAndDelete(job.redisValue())
+        jobRedisTemplate.opsForSet().remove(job.redisKey(), job.redisValue())
+        userRedisTemplate.opsForValue().getAndDelete(job.redisValue())
         return true
     }
 
     fun size(key: String): Long? {
-        return redisTemplate.opsForSet().size(key)
+        return jobRedisTemplate.opsForSet().size(key)
     }
 
     @Transactional
     fun isMember(job: Job): Boolean {
-        val isRedisValue = redisTemplate.opsForValue().get(job.redisValue())
+        val isRedisValue = userRedisTemplate.opsForValue().get(job.redisValue())
         if (isRedisValue != null) {
-            redisTemplate.opsForValue().set(job.redisValue(), job.redisValue(), job.workingTimeSec, TimeUnit.SECONDS)
             return true
         }
-        redisTemplate.opsForValue().getAndDelete(job.redisValue())
-        redisTemplate.opsForSet().remove(job.redisKey(), job.redisValue())
+        userRedisTemplate.opsForValue().getAndDelete(job.redisValue())
+        jobRedisTemplate.opsForSet().remove(job.redisKey(), job.redisValue())
         return false
     }
 }
