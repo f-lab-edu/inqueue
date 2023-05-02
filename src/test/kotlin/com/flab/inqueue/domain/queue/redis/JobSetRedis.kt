f6091675e1391b284fd3287c41eda7b680118895
JKjiwon <jwkim.oa@gmail.com>
2023-05-02 16:49:53 +0900

    test : 테스트 수정

--- src/test/kotlin/com/flab/inqueue/domain/queue/redis/JobSetRedis.kt
+++ src/test/kotlin/com/flab/inqueue/domain/queue/redis/JobSetRedis.kt
@@ -4,7 +4,6 @@ import com.flab.inqueue.TestContainer
 import com.flab.inqueue.domain.queue.entity.Job
 import com.flab.inqueue.domain.queue.entity.JobStatus
 import com.flab.inqueue.domain.queue.repository.JobRedisRepository
-import com.flab.inqueue.domain.queue.repository.WaitQueueRedisRepository
 import com.flab.inqueue.support.RedisConfigTest
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
@@ -15,45 +14,39 @@ import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 import org.springframework.data.redis.core.RedisTemplate
-import java.time.LocalDateTime
-import java.time.ZoneOffset
 import java.util.*
-
 
 
 @DataRedisTest
 @Import(RedisConfigTest::class)
 @ComponentScan(basePackages = ["com.flab.inqueue.domain.queue.repository"])
 class JobSetRedis @Autowired constructor(
-    private val waitQueueRedisRepository: WaitQueueRedisRepository,
     private val jobRedisRepository: JobRedisRepository,
-    private val redistemplate: RedisTemplate<String, String>,
-
-    ) : TestContainer() {
+    private val jobRedisTemplate: RedisTemplate<String, Job>,
+    private val userRedisTemplate: RedisTemplate<String, String>
+) : TestContainer() {
 
     private val logger = LoggerFactory.getLogger(JobSetRedis::class.java)
 
     @Test
     @DisplayName("redis set ttl 테스트")
     fun testRedisTTL() {
-        val expireInstant = LocalDateTime.now().plusSeconds(1).toInstant(ZoneOffset.ofHours(9))
-        val job = Job("testEventId", UUID.randomUUID().toString(), JobStatus.ENTER,expireInstant)
+        val job = Job("testEventId", UUID.randomUUID().toString(), JobStatus.ENTER, 1L)
 
         val startTime = System.currentTimeMillis()
         jobRedisRepository.register(job)
         logger.info("jobSetRedisRepository 저장 걸린시간 : {} ms ", System.currentTimeMillis() - startTime)
 
-        val existExpire1 = redistemplate.getExpire(job.redisKey())
-        val existExpire2 = redistemplate.getExpire(job.redisValue())
+        val existExpire1 = jobRedisTemplate.getExpire(job.redisKey)
+        val existExpire2 = userRedisTemplate.getExpire(job.redisValue)
 
-        logger.info("chech existExpire  : job.redisKey() = {}  , job.redisValue() = {} ", existExpire1, existExpire2)
+        logger.info("check existExpire  : job.redisKey() = {}  , job.redisValue() = {} ", existExpire1, existExpire2)
 
         Thread.sleep(2000L)
 
-        val expire1 = redistemplate.getExpire(job.redisKey())
-        val expire2 = redistemplate.getExpire(job.redisValue())
+        val expire1 = jobRedisTemplate.getExpire(job.redisKey)
+        val expire2 = userRedisTemplate.getExpire(job.redisValue)
 
-        logger.info("chech expire  : job.redisKey() = {}  , job.redisValue() = {} ", expire1, expire2)
+        logger.info("check expire  : job.redisKey() = {}  , job.redisValue() = {} ", expire1, expire2)
     }
-
 }
