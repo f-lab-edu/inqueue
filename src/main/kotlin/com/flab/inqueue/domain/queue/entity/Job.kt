bc4a4439718423ab4ca6a1da7d7856f50e7d8d54
JKjiwon <jwkim.oa@gmail.com>
2023-05-02 11:43:28 +0900

    refactor : 메소드를 프라퍼티로 변경

--- src/main/kotlin/com/flab/inqueue/domain/queue/entity/Job.kt
+++ src/main/kotlin/com/flab/inqueue/domain/queue/entity/Job.kt
@@ -1,15 +1,13 @@
 package com.flab.inqueue.domain.queue.entity
 
-import java.time.Instant
-
 class Job(
     val eventId: String,
     val userId: String,
     var status: JobStatus = JobStatus.WAIT,
-    val workingTimeSec : Long = 1L
+    val workingTimeSec: Long = 1L
 ) {
 
-    private val redisKey =  status.makeRedisKey(this.eventId)
-    fun redisKey(): String = redisKey
-    fun redisValue() : String = StringBuilder(redisKey).append(":").append(userId).toString()
+    val redisKey = status.makeRedisKey(eventId)
+    val redisValue = "${redisKey}:${userId}"
+
 }
