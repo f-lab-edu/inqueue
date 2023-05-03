15fdaf984d314da1ac86343ffa44d0040b1678b9
elyo9381 <won4413@daum.net>
2023-05-03 21:56:38 +0900

    fix : controller에 service 주입

--- src/main/kotlin/com/flab/inqueue/application/controller/EventController.kt
+++ src/main/kotlin/com/flab/inqueue/application/controller/EventController.kt
@@ -1,20 +1,23 @@
 package com.flab.inqueue.application.controller
 
-import com.flab.inqueue.domain.queue.dto.QueueInfo
-import com.flab.inqueue.domain.queue.dto.QueueResponse
 import com.flab.inqueue.domain.event.dto.EventRequest
 import com.flab.inqueue.domain.event.dto.EventResponse
 import com.flab.inqueue.domain.event.service.EventService
+import com.flab.inqueue.domain.queue.dto.QueueInfo
+import com.flab.inqueue.domain.queue.dto.QueueResponse
 import com.flab.inqueue.domain.queue.entity.JobStatus
+import com.flab.inqueue.domain.queue.service.JobService
+import com.flab.inqueue.security.common.CommonPrincipal
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
-import java.time.LocalTime
 
 @RestController
-@RequestMapping("/v1/events")
+@RequestMapping("/client/v1/events")
 class EventController(
     private val eventService: EventService,
+    private val jobService: JobService,
 ) {
     @PostMapping
     fun createEvent(
@@ -26,10 +29,12 @@ class EventController(
 
     @PostMapping("/{eventId}/enter")
     fun enterWaitQueue(
-        @RequestHeader("Authorization") accessToken: String,
         @PathVariable("eventId") eventId: String,
     ): QueueResponse {
-        return QueueResponse(JobStatus.WAIT, QueueInfo(1L, 1))
+        val principal = SecurityContextHolder.getContext().authentication.principal as CommonPrincipal
+        val userId = principal.userId ?: throw Exception("에러")
+        val enter = jobService.enter(eventId, userId)
+        return enter
     }
 
 
