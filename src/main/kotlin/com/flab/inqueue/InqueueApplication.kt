b9fc46d46320077796a1897fdf49c10452683047
JKjiwon <jwkim.oa@gmail.com>
2023-05-06 15:47:27 +0900

    feat: 대기열 -> 작업열 이동 스케쥴러 추가

--- src/main/kotlin/com/flab/inqueue/InqueueApplication.kt
+++ src/main/kotlin/com/flab/inqueue/InqueueApplication.kt
@@ -3,7 +3,9 @@ package com.flab.inqueue
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class InqueueApplication
