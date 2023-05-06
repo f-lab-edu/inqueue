2a8b3d9ba26ef5713a3687ea7dbc525cf139c76a
JKjiwon <jwkim.oa@gmail.com>
2023-05-06 15:47:27 +0900

    feat: 대기열 -> 작업열 이동 스케쥴러 추가

--- src/main/kotlin/com/flab/inqueue/domain/event/repository/EventRepository.kt
+++ src/main/kotlin/com/flab/inqueue/domain/event/repository/EventRepository.kt
@@ -2,7 +2,12 @@ package com.flab.inqueue.domain.event.repository
 
 import com.flab.inqueue.domain.event.entity.Event
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDateTime
 
 interface EventRepository : JpaRepository<Event, Long> {
     fun findByEventId(eventId: String): Event?
+
+    @Query("select event from Event event where event.period.startDateTime <= :bastTime and event.period.endDateTime >= :bastTime")
+    fun findOngoingEvents(bastTime: LocalDateTime): List<Event>
 }
