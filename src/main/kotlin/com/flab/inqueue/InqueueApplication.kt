c9bc3422b62cddcc3ffa3552b163fcca7b9a0aae
JKjiwon <33423123+JKjiwon@users.noreply.github.com>
2023-05-16 00:01:25 +0900

    Revert "refactor: runnable을 class로 분리, `@Async` 적용"
    This reverts commit fdc376968a4f4746ff670a47198bb182db57c5a8.

--- src/main/kotlin/com/flab/inqueue/InqueueApplication.kt
+++ src/main/kotlin/com/flab/inqueue/InqueueApplication.kt
@@ -3,10 +3,8 @@ package com.flab.inqueue
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableAsync
 @EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication
--- src/main/kotlin/com/flab/inqueue/domain/queue/scheduler/EnterJobScheduler.kt
+++ src/main/kotlin/com/flab/inqueue/domain/queue/scheduler/EnterJobScheduler.kt
@@ -1,6 +1,9 @@
 package com.flab.inqueue.domain.queue.scheduler
 
 import com.flab.inqueue.domain.event.repository.EventRepository
+import com.flab.inqueue.domain.queue.service.JobService
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.core.task.TaskExecutor
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
@@ -8,14 +11,29 @@ import java.time.LocalDateTime
 @Component
 class EnterJobScheduler(
     private val eventRepository: EventRepository,
-    private val enterJobScheduleTask: EnterJobScheduleTask
+    private val jobService: JobService,
+    @Qualifier("threadPoolTaskExecutor")
+    private val taskExecutor: TaskExecutor
 ) {
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(cron = "1 * * * * *")
     fun execute() {
         val events = eventRepository.findOngoingEvents(LocalDateTime.now())
 
         for (event in events) {
-            enterJobScheduleTask.enterJobs(event)
+            taskExecutor.execute(object : Runnable {
+                override fun run() {
+                    if (jobService.getWaitQueueSize(event) == 0L) {
+                        return
+                    }
+
+                    val availableJobQueueSize = jobService.getAvailableJobQueueSize(event)
+                    if (availableJobQueueSize <= 0) {
+                        return
+                    }
+
+                    jobService.enterAll(event, availableJobQueueSize)
+                }
+            })
         }
     }
 }
