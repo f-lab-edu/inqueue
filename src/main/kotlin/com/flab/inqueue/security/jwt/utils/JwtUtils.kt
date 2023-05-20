f06c5368f62c3a26d743892f82e5e47d105ef672
JKjiwon <jwkim.oa@gmail.com>
2023-05-20 22:11:33 +0900

    refactor : 불필요한 import문 제거

--- src/main/kotlin/com/flab/inqueue/security/jwt/utils/JwtUtils.kt
+++ src/main/kotlin/com/flab/inqueue/security/jwt/utils/JwtUtils.kt
@@ -3,7 +3,6 @@ package com.flab.inqueue.security.jwt.utils
 import io.jsonwebtoken.*
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.security.Key
 import java.time.ZoneId
